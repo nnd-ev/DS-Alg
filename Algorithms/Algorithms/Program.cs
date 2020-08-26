880b6fb7488f1feb5617bd6fa8d25578b5ff0932
nndev <nnd.stanojev@gmail.com>
2020-08-26 18:50:54 +0200

    Bubble sort - simple version

--- Algorithms/Algorithms/Program.cs
+++ Algorithms/Algorithms/Program.cs
@@ -7,6 +7,31 @@ using System.Threading.Tasks;
 namespace Algorithms {
     class Program {
         static void Main(string[] args) {
+            //bubble sort
+
+            int[] niz = { 3, 9, 1, 12, 99, 3, 28, 44, 1 };
+
+            Console.WriteLine("Original array: ");
+            for(int i=0;i<niz.Length; i++) {
+                Console.Write(niz[i] + ", ");
+            }
+
+            for(int i=0; i<niz.Length-1; i++) {
+                for(int j=i+1; j<niz.Length; j++) {
+                    if(niz[i] > niz[j]) { 
+                    int temp = niz[i];
+                    niz[i] = niz[j];
+                    niz[j] = temp;
+                }
+                }
+            }
+
+            Console.WriteLine("\nSorted array: ");
+
+            foreach (int item in niz) {
+                Console.Write(item + ", ");
+            }
+
         }
     }
 }
--- README.md
+++ README.md
@@ -1,3 +1,31 @@
 # algorithms
 
+//Sorting algorithms
 -->Bubble sort
+-->Merge sort
+-->Selection
+-->Shell sort
+-->Quick sort
+
+//Searching algorithms
+-->Binary serach
+-->Linear search
+-->Sequintial search
+
+
+
+//Matrix
+-->
+
+
+//
+Fibonacci
+Factorial
+Sum of digits
+Convert binary to Decimal
+Convert Decimal to binary
+Reverse number
+Merge and sort two arrays
+Convert hours --> sec
+Convert sec --> hours
+
