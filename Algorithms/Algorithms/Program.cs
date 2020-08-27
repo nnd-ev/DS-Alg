9e204ed9b59618b32f4cf9c8574b4bc3f259b8aa
nndev <nnd.stanojev@gmail.com>
2020-08-27 12:45:50 +0200

    quick sort

--- Algorithms/Algorithms/Program.cs
+++ Algorithms/Algorithms/Program.cs
@@ -7,28 +7,67 @@ using System.Threading.Tasks;
 namespace Algorithms {
     class Program {
 
-        
-    
-        static void Main(string[] args) {
-            //sequental search
+        private static void Quick_Sort(int[] arr, int left, int right) {
+            if (left < right) {
+                int pivot = Partition(arr, left, right);
 
-            int[] niz = { 2, 5, -4, 11, 0, 18, 22, 67, 51, 6 };
-
-            Console.WriteLine("Enter desired value: ");
-            int n = int.Parse(Console.ReadLine());
-
-            for(int i=0; i<niz.Length; i++) {
-                 
-                     if(niz[i] == n){
-                    Console.WriteLine("it's location is on {0}", i+1);
-                    return;
+                if (pivot > 1) {
+                    Quick_Sort(arr, left, pivot - 1);
                 }
-               else if(i == niz.Length - 1) {
-                    Console.WriteLine("There is not such element in array.");
-                } 
-                   
+                if (pivot + 1 < right) {
+                    Quick_Sort(arr, pivot + 1, right);
+                }
             }
 
         }
+
+        private static int Partition(int[] arr, int left, int right) {
+            int pivot = arr[left];
+            while (true) {
+
+                while (arr[left] < pivot) {
+                    left++;
+                }
+
+                while (arr[right] > pivot) {
+                    right--;
+                }
+
+                if (left < right) {
+                    if (arr[left] == arr[right]) return right;
+
+                    int temp = arr[left];
+                    arr[left] = arr[right];
+                    arr[right] = temp;
+
+
+                } else {
+                    return right;
+                }
+            }
+        }
+        static void Main(string[] args) {
+            //quick sort
+
+            int[] niz = { 2, 5, -4, 11, 0, 18, 22, 67, 51, 6 };
+
+            Console.WriteLine("Original array : ");
+            foreach (var item in niz) {
+                Console.Write(" " + item);
+            }
+            Console.WriteLine();
+
+            Quick_Sort(niz, 0, niz.Length - 1);
+
+            Console.WriteLine();
+            Console.WriteLine("Sorted array : ");
+
+            foreach (var item in niz) {
+                Console.Write(" " + item);
+            }
+            Console.WriteLine();
+        }
     }
+
+
 }
