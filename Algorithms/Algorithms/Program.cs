55e7fc00d031137219f58913b2b51f879e9905ee
nndev <nnd.stanojev@gmail.com>
2020-08-27 20:41:42 +0200

    Shell sort

--- Algorithms/Algorithms/Program.cs
+++ Algorithms/Algorithms/Program.cs
@@ -11,37 +11,35 @@ namespace Algorithms {
         
     
         static void Main(string[] args) {
-            //Insertion sort
+            //Shell sort
 
             int[] niz = { 2, 5, -4, 11, 0, 18, 22, 67, 51, 6 };
-            //Console.WriteLine("Enter the number which you want to be searched");
-            //int number = int.Parse(Console.ReadLine());
+            
             Console.WriteLine("Original array: ");
             foreach (int item in niz) {
                 Console.Write(item + " ");
             }
-
-
-            for (int i=1; i<niz.Length; i++) {
-                int temp = niz[i]; //tem=1
-                int j= i - 1; //j=0
-
-                while(j>=0 && temp <= niz[j]) {
-                    niz[j + 1] = niz[j];
-                    j = j - 1;
+            int len = niz.Length;
+            int i, j, k, tmp;
+            for (i = len / 2; i > 0; i = i / 2) {
+                for (j = i; j < len; j++) {
+                    for (k = j - i; k >= 0; k = k - i) {
+                        if (niz[k + i] >= niz[k])
+                            break;
+                        else {
+                            tmp = niz[k];
+                            niz[k] = niz[k + i];
+                            niz[k + i] = tmp;
+                        }
+                    }
+                }
                 }
 
-                niz[j + 1] = temp;
-            }
 
-            Console.WriteLine("Soreted array: ");
-            foreach(int item in niz) {
+                Console.WriteLine("Soreted array: ");
+            foreach (int item in niz) {
                 Console.Write(item + " ");
-            }
- 
-
-
-
+            } 
         }
     }
 }
