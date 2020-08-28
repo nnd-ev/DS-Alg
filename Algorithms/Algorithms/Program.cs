97deb202f6140fb988d6136a180afe8768654c5e
nndev <nnd.stanojev@gmail.com>
2020-08-28 15:56:09 +0200

    Binary search - wile version

--- Algorithms/Algorithms/Program.cs
+++ Algorithms/Algorithms/Program.cs
@@ -8,38 +8,47 @@ namespace Algorithms {
     class Program {
 
        
+        private static int binarySearch(int[] niz, int len, int number) {
+            int first, last, mid;
+            first = 0;
+            last = len - 1;
+
+            while(first <= last) {
+                mid = (first + last) / 2;
+                if (niz[mid] == number)
+                    return mid;
+                if (number < niz[mid])
+                    last = mid + 1;
+                else
+                    first = mid + 1;
+            }
+
+            return -1;
+        }
         
     
         static void Main(string[] args) {
-            //Shell sort
+            //Binary search-while way
 
-            int[] niz = { 2, 5, -4, 11, 0, 18, 22, 67, 51, 6 };
-            
+            //int[] niz = { 2, 5, -4, 11, 0, 18, 22, 67, 51, 6 };
+            int[] niz = {1,2,3,4,5,6,7,8};
+
             Console.WriteLine("Original array: ");
             foreach (int item in niz) {
                 Console.Write(item + " ");
             }
-            int len = niz.Length;
-            int i, j, k, tmp;
-            for (i = len / 2; i > 0; i = i / 2) {
-                for (j = i; j < len; j++) {
-                    for (k = j - i; k >= 0; k = k - i) {
-                        if (niz[k + i] >= niz[k])
-                            break;
-                        else {
-                            tmp = niz[k];
-                            niz[k] = niz[k + i];
-                            niz[k + i] = tmp;
-                        }
-                    }
-                }
-                }
+
+            Console.WriteLine("What number's index you wanna find? ");
+            int number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Position of searched element is at: "+ binarySearch(niz, niz.Length, number));
+            
 
 
-                Console.WriteLine("Soreted array: ");
-            foreach (int item in niz) {
-                Console.Write(item + " ");
-            } 
+            //    Console.WriteLine("Soreted array: ");
+            //foreach (int item in niz) {
+            //    Console.Write(item + " ");
+            //} 
         }
     }
 }
